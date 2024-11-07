7670dac9c9c4a07558400f76c77268e383526381
Eric Alarcon Dolynko <e.alarcond@uniandes.edu.co>
2024-11-07 00:41:33 -0500

    Intento again

--- check-pagos-kong.py
+++ check-pagos-kong.py
@@ -1,12 +1,17 @@
-from ofipensiones.settings import EMAIL_HOST_USER
+import os
+import django
 from django.core.mail import send_mail
 import requests
 import time
+from ofipensiones.settings import EMAIL_HOST_USER
+
+# Configura el entorno de Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ofipensiones.settings")
+django.setup()
 
 def check_kong_status():
     try:
         response = requests.get("http://10.128.0.51:8000/pagos/health-check")
-
         return response.status_code == 200
     except requests.ConnectionError:
         return False
