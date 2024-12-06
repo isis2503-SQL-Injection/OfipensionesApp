e62f7d74d606e974d80c3db086a1fabedb060dc1
MATEO_LOPEZ <m.lopez112@uniandes.edu.co>
2024-12-06 10:00:02 -0500

    mostrar

--- patron/master/descuento/views.py
+++ patron/master/descuento/views.py
@@ -7,36 +7,6 @@ from django.utils.dateparse import parse_date
 from django.http import JsonResponse
 
 
-def revisarDescuentos(request):
-    if request.method == 'GET':
-        codigo_estudiante = request.GET.get('codigoEstudiante')
-
-        if not codigo_estudiante:
-            return render(request, 'reporteDescuento.html', {'error': 'Falta el estudiante'})
-        try:
-
-            estudiante = Estudiante.objects.get(codigoEstudiante=codigo_estudiante)
-            descuentos = Descuento.objects.filter(codigo_estudiante=codigo_estudiante)
-            
-        
-            Informacion_descuento = [
-                    {
-                        'fecha_emision': desc.mes,
-                        'Porcentaje de descuento': desc.porcentage
-                    }
-                    for desc in descuentos
-                ]
-            return render(request, 'reporteEstadoCuenta.html', {
-                'codigo_estudiante': estudiante.codigoEstudiante,
-                'reportes': Informacion_descuento,
-
-            })
-
-        except Estudiante.objects.get(codigoEstudiante=codigo_estudiante).DoesNotExist:
-            return render(request, 'reporteEstadoCuenta.html', {'error': 'Estudiante no encontrado'})
-
-    return render(request, 'reporteEstadoCuenta.html', {'error': 'Método no permitido'})
-
 def nuevo_descuento(request):
     if request.method == "POST":
         try:
